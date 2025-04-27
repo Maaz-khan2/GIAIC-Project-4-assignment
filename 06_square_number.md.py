56919bf1db117edbadb5a8838bdefa331df55f4c
Maaz khan <161418448+Maaz-khan2@users.noreply.github.com>
2025-04-27 17:31:32 +0500

    Add files via upload

---
+++ 06_square_number.md.py
@@ -0,0 +1,25 @@
+# Problem Statement
+# Ask the user for a number and print its square (the product of the number times itself).
+
+# Here's a sample run of the program (user input is in bold italics):
+
+# Type a number to see its square: 4
+
+# 4.0 squared is 16.0
+
+
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    num1 : float = float(input("Type a number to see it square:"))
+    print(str(num1) + "squared is" + str(num1 ** 2))
+
+if __name__ == '__main__':
+    main()         
